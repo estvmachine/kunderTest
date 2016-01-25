f4cb95718b2a944d66a70874347871160bd837e1
EstebanCampos <campos.esteban@gmail.com>
2016-01-25 10:23:28 -0300

    Se vuelve al estado anterior con http, no muestra con https tampoco en kunder

--- www/js/app.js
+++ www/js/app.js
@@ -107,5 +107,5 @@ angular.module('starter', ['ionic', 'ionic-material' , 'starter.controllers', 's
 
   });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/search');
 });
